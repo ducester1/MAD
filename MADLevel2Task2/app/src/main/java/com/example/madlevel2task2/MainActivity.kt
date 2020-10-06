db26f2af1d70d5508864fc01261fbfb44e1aba63
Scott <schepers.scott@hotmail.com>
2020-10-06 16:02:07 +0200

    Setup recyclerview

--- MADLevel2Task2/app/src/main/java/com/example/madlevel2task2/MainActivity.kt
+++ MADLevel2Task2/app/src/main/java/com/example/madlevel2task2/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.madlevel2task2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.madlevel2task2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val questions = arrayListOf<Question>()
+    private val questionAdapter = QuestionAdapter(questions)
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        binding.rvQuestions.layoutManager = LinearLayoutManager(this@MainActivity, RecyclerView.VERTICAL, false)
+        binding.rvQuestions.adapter = questionAdapter
+
+        binding.rvQuestions.addItemDecoration(DividerItemDecoration(this@MainActivity, DividerItemDecoration.VERTICAL))
+
+        for (i in Question.QUESTIONS.indices) {
+            questions.add(Question(Question.QUESTIONS[i]))
+        }
+        questionAdapter.notifyDataSetChanged()
     }
 }
---
+++ MADLevel2Task2/app/src/main/java/com/example/madlevel2task2/Question.kt
@@ -0,0 +1,14 @@
+package com.example.madlevel2task2
+
+data class Question(
+    var questionText: String
+) {
+    companion object {
+        val QUESTIONS = arrayOf(
+            "A 'val' and 'var' are the same.",
+            "Mobile Application Development grants 12 ECTS.",
+            "A Unit in Kotlin corresponds to a void in Java.",
+            "In Kotlin 'when' replaces the 'switch' operator in Java."
+        )
+    }
+}
