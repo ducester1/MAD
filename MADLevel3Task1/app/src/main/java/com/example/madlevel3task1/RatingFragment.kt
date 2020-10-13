4a3b62adf8417b9193091450eab109a36a21bdd6
Scott <schepers.scott@hotmail.com>
2020-10-13 20:47:09 +0200

    Step 4

--- MADLevel3Task1/app/src/main/java/com/example/madlevel3task1/RatingFragment.kt
+++ MADLevel3Task1/app/src/main/java/com/example/madlevel3task1/RatingFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_rating.*
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -21,5 +23,25 @@ class RatingFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        btn_summary.setOnClickListener {
+            navigateToSummery()
+        }
+        showRandomAssessableGame()
+    }
+
+    private fun showRandomAssessableGame() {
+        val randomGame = listOf("Red Dead Redemption 2", "Rocket League",
+            "Shadow of the Tombraider").random()
+
+        tv_game.text = randomGame
+    }
+
+    private fun navigateToSummery() {
+        val args = Bundle()
+        args.putFloat(ARG_GAME_RATING, rb_game.rating)
+        args.putString(ARG_GAME_NAME, tv_game.text.toString())
+
+        findNavController().navigate(R.id.action_ratingFragment_to_summaryFragment, args)
     }
 }
