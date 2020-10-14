e081d61bc4df9807ff960b81051ad1d0e04edf43
Scott <schepers.scott@hotmail.com>
2020-10-15 01:12:57 +0200

    Added: now opens the clicked portal in chrome (completed)

--- MADLevel3Task2/app/src/main/java/com/example/madlevel3task2/PortalsFragment.kt
+++ MADLevel3Task2/app/src/main/java/com/example/madlevel3task2/PortalsFragment.kt
@@ -1,5 +1,7 @@
 package com.example.madlevel3task2
 
+import CustomTabHelper
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.GridLayout
+import androidx.browser.customtabs.CustomTabsIntent
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import kotlinx.android.synthetic.main.fragment_portals.*
@@ -15,6 +18,7 @@ private var portals: ArrayList<Portal> = arrayListOf()
 
 class PortalsFragment : Fragment() {
 
+    private var customTabHelper: CustomTabHelper = CustomTabHelper()
     private var portalAdapter: PortalAdapter = PortalAdapter(portals) { portal : Portal -> portalItemClicked(portal)}
 
     override fun onCreateView(
@@ -45,6 +49,15 @@ class PortalsFragment : Fragment() {
     }
 
     private fun portalItemClicked(portal: Portal) {
-        println("Clicked " + portal.title + " , from " + portal.url)
+        val builder = CustomTabsIntent.Builder()
+        val customTabsIntent = builder.build()
+        val packageName = customTabHelper.getPackageNameToUse(this.requireContext(), portal.url.toString())
+
+        if (packageName == null) {
+            println("Empty package name")
+        } else {
+            customTabsIntent.intent.setPackage(packageName)
+            customTabsIntent.launchUrl(this.requireContext(), Uri.parse(portal.url))
+        }
     }
 }
