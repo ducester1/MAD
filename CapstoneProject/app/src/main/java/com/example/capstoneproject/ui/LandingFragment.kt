5adb1670d3b197e16e67fa634908450d6e8fb9c7
Scott <schepers.scott@hotmail.com>
2020-12-16 09:31:32 +0100

    Finished landing page XML

--- CapstoneProject/app/src/main/java/com/example/capstoneproject/ui/LandingFragment.kt
+++ CapstoneProject/app/src/main/java/com/example/capstoneproject/ui/LandingFragment.kt
@@ -1,31 +1,39 @@
-package com.example.capstoneproject
+package com.example.capstoneproject.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import com.example.capstoneproject.R
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class LandingFragment : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
+        setHasOptionsMenu(true)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_landing, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        view.findViewById<Button>(R.id.btnWhatCanUMake).setOnClickListener {
+            //findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu) {
+        activity?.title = getString(R.string.landing_fragment_title)
+        super.onPrepareOptionsMenu(menu)
     }
 }
